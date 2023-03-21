cce200a77efa3636c2a8038a7cd8195558c0aca3
vvoloshchuk-corcentric <vvoloshchuk@corcentric.com>
2023-03-21 13:07:20 +0100

    improvements

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # Check Conventional Commits GitHub Action
 
-A simple GitHub action that checks commits to have at least one
-message following [Conventional Commits](https://www.conventionalcommits.org/en/v1.0.0/) specification.
+A simple GitHub action that checks commits to have at least one message
+following [Conventional Commits](https://www.conventionalcommits.org/en/v1.0.0/) specification.
+
+### Prepare to publish
+- make changes
+- build project
+- commit and push changes
+- create and push tag
+- publish release with tag
+
+### Build
+```bash
+NODE_OPTIONS=--openssl-legacy-provider npm run build
+```
 
 ### Usage
 
@@ -20,5 +32,7 @@ jobs:
     runs-on: ubuntu-latest
     steps:
       - uses: actions/checkout@v2
-      - uses: vvoloshchuk777/check-conventional-commits-action@v1.0.0
+      - uses: Determine-Corp/check-conventional-commits-action@v1.0.0
+        with:
+          GITHUB_TOKEN: ${{secrets.GITHUB_TOKEN}}
 ```
--- src/fetchCommits.ts
+++ src/fetchCommits.ts
@@ -1,27 +1,33 @@
-import get from 'lodash.get';
-import { request } from 'undici';
+import axios from 'axios';
 import { Commit } from './types';
+import { debug, error, getInput } from '@actions/core';
 
 const fetchCommits = async (context): Promise<Commit[]> => {
   // push commits
-  const commits = Array.isArray(get(context, 'payload.commits'));
+  const commits = Array.isArray(context?.payload?.commits);
   if (commits) {
     return context.payload.commits;
   }
 
   // PR commits
   // Get a list of commits from the GH API:
-  const commitsURL = get(context, 'payload.pull_request.commits_url');
+  const commitsURL = context.payload.pull_request.commits_url;
+  debug(`url ${commitsURL}`);
   if (commitsURL) {
     try {
-      const { body } = await request(commitsURL);
-
-      const data = await body;
+      const { data } = await axios.get(commitsURL, {
+        headers: {
+          Accept: 'application/vnd.github+json',
+          Authorization: `token ${getInput('GITHUB_TOKEN')}`
+        }
+      });
 
       if (Array.isArray(data)) {
         return data.map(item => item.commit);
       }
     } catch (e) {
+      error(`Error: ${e}`);
+
       return [];
     }
   }
